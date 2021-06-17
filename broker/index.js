9e11529ff6a519587bfd0e1c791e975d08ccb934
Andres <andres857@gmail.com>
2021-06-16 21:44:22 -0500

    the player id is obtained dynamically

--- broker/index.js
+++ broker/index.js
@@ -1,26 +1,38 @@
 require('dotenv').config();
 const MQTT = require("async-mqtt");
+const {serialPlayer} = require('../infosystem')
+
 
 const clientPlayer = process.env.CLIENT
 const salaEspera = process.env.SALA_DE_ESPERA
 const tv = process.env.TV
 const serverBroker = process.env.SERVERBROKER
-const idplayer = process.env.IDPLAYER
 
-const options = {
-    connectTimeout:4000,
-    clientId: `${clientPlayer}/player/${salaEspera}/${tv}/ddcef5209dc54d318d0afc859a42b7c2`,
-    username:'emqx',
-    password: 'public',
-    keepalive:60,
-    clean:true
+
+
+async function buildOptions(){
+    let idPlayer = await serialPlayer()
+    const options = {
+        connectTimeout:4000,
+        clientId: `${clientPlayer}/player/${salaEspera}/${tv}/${idPlayer}`,
+        username:'emqx',
+        password: 'public',
+        keepalive:60,
+        clean:true
+    }
+    return options
 }
+
 
 // console.log(options.clientId);
 
 async function getClient(){
+    let options = await buildOptions()
+    console.log(options);
     return await MQTT.connectAsync(`mqtt://${serverBroker}`,options)
 }
+
+getClient()
 
 module.exports={
     getClient
